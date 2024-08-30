e7199d723149c3a40bc90c55944aa711b8bd155e
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-08-30 23:38:05 +0200

    sdkj

--- src/stacks/my-stack.ts
+++ src/stacks/my-stack.ts
@@ -1,21 +1,54 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { SecretValue, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import {
   AttributeType,
   StreamViewType,
   TableV2,
 } from "aws-cdk-lib/aws-dynamodb";
+import { Connection, Authorization } from "aws-cdk-lib/aws-events";
 import { Construct } from "constructs";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // Define the DynamoDB table for the weather stats demo
-    new TableV2(this, "weather-stats-demo-table", {
-      tableName: "weather-stats-demo",
+    // Read MOMENTO_API_KEY and MOMENTO_API_ENDPOINT from environment variables
+    const momentoApiKey = process.env.MOMENTO_API_KEY;
+    const momentoApiEndpoint = process.env.MOMENTO_API_ENDPOINT;
+    if (!momentoApiKey || !momentoApiEndpoint) {
+      throw new Error(
+        "Required environment variables 'MOMENTO_API_KEY' or 'MOMENTO_API_ENDPOINT' are missing or undefined",
+      );
+    }
+
+    const momentoApiKeySecret = new Secret(this, "MomentoApiKeySecret", {
+      secretName: "momento-api-key-secret",
+      secretStringValue: SecretValue.unsafePlainText(momentoApiKey),
+    });
+
+    /*
+     * DYNAMODB
+     */
+
+    new TableV2(this, "WeatherStatsTable", {
+      tableName: "weather-stats-table",
       partitionKey: { name: "Location", type: AttributeType.STRING },
       dynamoStream: StreamViewType.NEW_AND_OLD_IMAGES,
       removalPolicy: RemovalPolicy.DESTROY,
     });
+
+    /*
+     * EVENTBRIDGE
+     */
+
+    new Connection(this, "WeatherStatsConnection", {
+      connectionName: "weather-stats-connection",
+      authorization: Authorization.apiKey(
+        "Momento API Key",
+        SecretValue.secretsManager(momentoApiKeySecret.secretName),
+      ),
+    });
+
+    // ... rest of the code ...
   }
 }
