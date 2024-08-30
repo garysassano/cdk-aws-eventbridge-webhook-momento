bc0ebee929836be20e3f175dc35d713739b3ac7b
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-08-30 22:52:01 +0200

    update

--- src/stacks/my-stack.ts
+++ src/stacks/my-stack.ts
@@ -1,47 +1,17 @@
-import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
-import {
-  EventAction,
-  FilterGroup,
-  GitHubSourceCredentials,
-  Project,
-  Source,
-} from "aws-cdk-lib/aws-codebuild";
-import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Table, AttributeType, StreamViewType } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // Read GITHUB_TOKEN, GITHUB_REPO, and GITHUB_OWNER from environment variables
-    const githubToken = process.env.GITHUB_TOKEN;
-    const githubRepo = process.env.GITHUB_REPO;
-    const githubOwner = process.env.GITHUB_OWNER;
-    if (!githubToken || !githubRepo || !githubOwner) {
-      throw new Error(
-        "Required environment variables 'GITHUB_TOKEN', 'GITHUB_REPO', or 'GITHUB_OWNER' are missing or undefined",
-      );
-    }
-
-    const githubTokenSecret = new Secret(this, "GithubTokenSecret", {
-      secretName: "github-token",
-      secretStringValue: SecretValue.unsafePlainText(githubToken),
-    });
-
-    new GitHubSourceCredentials(this, "GithubSourceCredentials", {
-      accessToken: SecretValue.secretsManager(githubTokenSecret.secretArn),
-    });
-
-    new Project(this, "SampleProject", {
-      projectName: "sample-project",
-      source: Source.gitHub({
-        owner: githubOwner,
-        repo: githubRepo,
-        webhook: true,
-        webhookFilters: [
-          FilterGroup.inEventOf(EventAction.WORKFLOW_JOB_QUEUED),
-        ],
-      }),
+    // Define the DynamoDB table for the weather stats demo
+    const weatherStatsDemoTable = new Table(this, "weather-stats-demo-table", {
+      tableName: "weather-stats-demo",
+      partitionKey: { name: "Location", type: AttributeType.STRING },
+      stream: StreamViewType.NEW_AND_OLD_IMAGES,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
   }
 }
