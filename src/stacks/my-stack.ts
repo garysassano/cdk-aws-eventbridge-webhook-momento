910d0be3bd7b9f679085dea6c9fc55bc4b9396f1
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-08-31 17:45:14 +0200

    sdkjf

--- src/stacks/my-stack.ts
+++ src/stacks/my-stack.ts
@@ -1,4 +1,10 @@
-import { SecretValue, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  SecretValue,
+  RemovalPolicy,
+  Stack,
+  StackProps,
+  Duration,
+} from "aws-cdk-lib";
 import {
   AttributeType,
   StreamViewType,
@@ -12,6 +18,7 @@ import {
 } from "aws-cdk-lib/aws-events";
 import { Construct } from "constructs";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { CfnPipe } from "aws-cdk-lib/aws-pipes";
 // import {
 //   Pipe,
 //   FilterPattern,
@@ -46,7 +53,7 @@ export class MyStack extends Stack {
      * DYNAMODB
      */
 
-    new TableV2(this, "WeatherStatsTable", {
+    const weatherStatsTable = new TableV2(this, "WeatherStatsTable", {
       tableName: "weather-stats-table",
       partitionKey: { name: "Location", type: AttributeType.STRING },
       dynamoStream: StreamViewType.NEW_AND_OLD_IMAGES,
@@ -87,129 +94,7 @@ export class MyStack extends Stack {
     });
 
     /*
-     * EVENTBRIDGE PIPES
+     * TO MODIFY FROM HERE
      */
-
-    // const source = new DynamodbStreamSource(weatherStatsDemoTable, {
-    //   batchSize: 1,
-    //   startingPosition: pipes.StartingPosition.LATEST,
-    //   maximumRetryAttempts: 0,
-    //   onFailure: pipes.SourceFailureMode.SEND_TO_QUEUE,
-    //   deadLetterQueue: deadLetterQueue,
-    // });
-
-    // // Define the dead letter queue for inspecting failed events to event bridge
-    // const deadLetterQueue = new sqs.Queue(this, "DeadLetterQueue", {
-    //   queueName: "weather-stats-demo-dlq",
-    //   retentionPeriod: Duration.days(14),
-    // });
-
-    // // Define the role for the event bridge
-    // const role = new iam.Role(
-    //   this,
-    //   "AmazonEventBridgePipeWeatherStatsDemoEventToMomentoCache",
-    //   {
-    //     roleName: "AmazonEventBridgePipeWeatherStatsDemoEventToMomentoCache",
-    //     assumedBy: new iam.ServicePrincipal("pipes.amazonaws.com"),
-    //   },
-    // );
-    // this.addPolicyForEventBridgeRole(
-    //   role,
-    //   cachePutApiDestination,
-    //   cacheDeleteApiDestination,
-    //   topicPublishApiDestination,
-    //   weatherStatsDemoTable,
-    //   deadLetterQueue,
-    // );
-
-    // // Create a log group for the pipes
-    // const logGroup = new logs.LogGroup(this, "PipesLogGroup", {
-    //   logGroupName: "/aws/pipes/weather-stats-demo",
-    //   retention: logs.RetentionDays.TWO_WEEKS,
-    //   removalPolicy: RemovalPolicy.DESTROY,
-    // });
-
-    // // Define the pipe for the cache put operation
-    // const cachePutPipe = new Pipe(this, "WeatherStatsDemoCachePutPipe", {
-    //   pipeName: "weather-stats-demo-cache-put-pipe",
-    //   source: source,
-    //   target: new pipes.ApiDestinationTarget(cachePutApiDestination, {
-    //     inputTransformation: InputTransformation.fromObject({
-    //       Location: DynamicInput.fromEventPath("$.dynamodb.Keys.Location.S"),
-    //       MaxTemp: DynamicInput.fromEventPath("$.dynamodb.NewImage.MaxTemp.N"),
-    //       MinTemp: DynamicInput.fromEventPath("$.dynamodb.NewImage.MinTemp.N"),
-    //       ChancesOfPrecipitation: DynamicInput.fromEventPath(
-    //         "$.dynamodb.NewImage.ChancesOfPrecipitation.N",
-    //       ),
-    //     }),
-    //     httpParameters: {
-    //       pathParameterValues: [cacheName],
-    //       queryStringParameters: {
-    //         key: DynamicInput.fromEventPath("$.dynamodb.Keys.Location.S"),
-    //         ttl_seconds: DynamicInput.fromEventPath(
-    //           "$.dynamodb.NewImage.TTL.N",
-    //         ),
-    //       },
-    //     },
-    //   }),
-    //   role: role,
-    //   filter: new pipes.Filter([
-    //     FilterPattern.fromObject({ eventName: ["INSERT", "MODIFY"] }),
-    //   ]),
-    // });
-
-    // // Define the pipe for the topic publish operation
-    // const topicPublishPipe = new Pipe(
-    //   this,
-    //   "WeatherStatsDemoTopicPublishPipe",
-    //   {
-    //     pipeName: "weather-stats-demo-topic-publish-pipe",
-    //     source: source,
-    //     target: new pipes.ApiDestinationTarget(topicPublishApiDestination, {
-    //       inputTransformation: InputTransformation.fromObject({
-    //         EventType: DynamicInput.fromEventPath("$.eventName"),
-    //         Location: DynamicInput.fromEventPath("$.dynamodb.Keys.Location.S"),
-    //         MaxTemp: DynamicInput.fromEventPath(
-    //           "$.dynamodb.NewImage.MaxTemp.N",
-    //         ),
-    //         MinTemp: DynamicInput.fromEventPath(
-    //           "$.dynamodb.NewImage.MinTemp.N",
-    //         ),
-    //         ChancesOfPrecipitation: DynamicInput.fromEventPath(
-    //           "$.dynamodb.NewImage.ChancesOfPrecipitation.N",
-    //         ),
-    //       }),
-    //       httpParameters: {
-    //         pathParameterValues: [cacheName, topicName],
-    //       },
-    //     }),
-    //     role: role,
-    //   },
-    // );
-
-    // // Define the pipe for the cache delete operation
-    // const cacheDeletePipe = new Pipe(this, "WeatherStatsDemoCacheDeletePipe", {
-    //   pipeName: "weather-stats-demo-cache-delete-pipe",
-    //   source: source,
-    //   target: new pipes.ApiDestinationTarget(cacheDeleteApiDestination, {
-    //     httpParameters: {
-    //       pathParameterValues: [cacheName],
-    //       queryStringParameters: {
-    //         key: DynamicInput.fromEventPath("$.dynamodb.Keys.Location.S"),
-    //       },
-    //     },
-    //   }),
-    //   role: role,
-    //   filter: new pipes.Filter([
-    //     FilterPattern.fromObject({ eventName: ["REMOVE"] }),
-    //   ]),
-    // });
-
-    // // Configure logging for all pipes
-    // [cachePutPipe, topicPublishPipe, cacheDeletePipe].forEach((pipe) => {
-    //   pipe.addLogDestination(new pipes.CloudWatchLogDestination(logGroup));
-    //   pipe.logLevel = pipes.LogLevel.INFO;
-    //   pipe.logIncludeExecutionData = [pipes.IncludeExecutionData.ALL];
-    // });
   }
 }
