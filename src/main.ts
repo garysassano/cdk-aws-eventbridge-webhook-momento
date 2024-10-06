43672c1ef1487a6746369cc20dcd88abc1fdc023
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-10-06 08:28:50 +0200

    lll

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { App } from "aws-cdk-lib";
-// import { MyStack } from "./stacks/my-stack";
-import { TestStack } from "./stacks/test-stack";
+import { MyStack } from "./stacks/my-stack";
 
 // for development, use account/region from cdk cli
 const devEnv = {
